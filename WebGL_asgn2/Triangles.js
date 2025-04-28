9a45874c1b51d2e1a7f41f125782c38335321206
Opal Oliver Minor <42308423+O-Minor@users.noreply.github.com>
2025-04-28 00:40:16 -0700

    47→ 58 fps buffer performance improvement
    buffer aspect of performance improved learned that and thing === null arent the same and using !thing fixed it. camera still flickers when moves

--- WebGL_asgn2/Triangles.js
+++ WebGL_asgn2/Triangles.js
@@ -4,6 +4,9 @@ class Triangle{
         this.position = [0.0, 0.0, 0.0];
         this.color = [1.0, 1.0, 1.0, 1.0];
         this.size = 5.0;
+
+        this.buffer = null; // !
+        this.vertices = null; // !
     }
     render(){
         var xy = this.position;
@@ -67,14 +70,37 @@ function drawTriangle3D(vertices) {
     var n = 3; // The number of vertices
   
     // Create a buffer object on GPU
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
+    // ! means performance optimized
+    if (!this.buffer){
+        this.buffer = gl.createBuffer();
+        if (!this.buffer) {
+            console.log('Failed to create the buffer object');
+            return -1;
+        }
+    }
+    if (this.buffer === null){
+        this.buffer = gl.createBuffer();
+        if (!this.buffer) {
+            console.log('Failed to create the buffer object');
+            return -1;
+        }
+    }
+    if (!this.buffer) {
+        console.error('Failed to create the buffer object');
+        return -1;
     }
 
+    // $ means og that worked
+    // var vertexBuffer = gl.createBuffer();
+    // if (!vertexBuffer) {
+    //   console.log('Failed to create the buffer object');
+    //   return -1;
+    // }
+
     // Bind the buffer object to target
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    // gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer); // $
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer); // !
+    
     // Write date into the buffer object aka send to GPU
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW);
     // atrib pointer makes it so you can pass multiple things (now 3 for x y z)
